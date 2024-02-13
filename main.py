ca477061324cf81717e2cea9fcef7909c1cc32db
Sherajul Islam <SHERAJUL.ISLAM@icddrb.net>
2024-02-13 13:17:57 +0600

    Full functioning code for auth and todos

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+import models
+from database import engine
+from routers import auth, todos, admin, users
+
+app = FastAPI()
+
+models.Base.metadata.create_all(bind=engine)
+
+app.include_router(auth.router)
+app.include_router(todos.router)
+app.include_router(admin.router)
+app.include_router(users.router)
+
+# make venv -> python -m venv venv
+# activate -> source venv/Scripts/activate
+# make txt file -> pip freeze > requirements.txt
+# pip install -r requirements.txt
+# uvicorn main:app --reload
